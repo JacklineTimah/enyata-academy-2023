90825cc988ddd84b2366335a1b095dd79d43aa16
JACKLINE TIMAH <jacklinetimah57@gmail.com>
2023-05-26 08:18:08 +0000

    javascript assignment

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,22 @@
+//Solution 1
+let myName= "Jackline";
+
+//Solution 2
+console.log(typeof myName);// Output: "string"
+
+//Solution 3
+myName= "Timah";
+
+//Solution 4( 10 Properties of Car)
+let Car = {
+    CarName:"V8" ,
+    Model: "Toyota Land Cruiser V8",
+    Year of manufacture: 2008,
+    Price: "85,840USD globally",
+    Engine type: "5.7 liter V8 engine",
+    Color: "Available in a variety of colors",
+    Number of wheels: "Four wheel drive",
+    Features:"an eight-speed automatic transmission",
+    Expensive:"Yes",
+    Fuel Consumption:"compatible with a variety of fuel types but gas models are most costly to run", 
+}
---
+++ 0x01-intro_to_js/number.js
@@ -0,0 +1,17 @@
+//Question1 Answer
+const favoriteNumber= 4 ;
+
+//Question2 Answer
+const halfofFavNum= favoriteNumber/2;
+
+//Question3 Answer
+let isaacsFavorite= 13;
+
+//Question4 Answer
+const difference = isaacsFavorite - favoriteNumber;
+
+//Question5 Answer
+isaacsFavorite = isaacsFavorite * 26;
+
+// Question6 Answer 
+isaacsFavorite %= 3;
